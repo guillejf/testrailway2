b15e1423602aed926821338f84ea0111ea5c3ff6
guillejf <maildeguille@gmail.com>
2023-02-02 21:06:28 -0300

    v1

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+
+if (process.env.MODE != "production") {
+  require("dotenv").config();
+}
+
+const PORT = process.env.PORT;
+const MODE = process.env.MODE;
+const MONGO_URL = process.env.MONGO_URL;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    PORT,
+    MODE,
+    MONGO_URL,
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log({
+    PORT,
+    MODE,
+    MONGO_URL,
+  });
 });
